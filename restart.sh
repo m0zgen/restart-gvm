21c81ebbb5bc256bf4f2b9c491073c1d1217e4a6
/:) <g.yevgeniy.p@gmail.com>
2021-09-07 17:05:00 +0300

    + funx

--- restart.sh
+++ restart.sh
@@ -2,6 +2,10 @@
 # Created by Y.G., https://sys-adm.in 
 # Simple restart OpenVAS script
 
-systemctl restart gsad.service
-systemctl restart ospd-openvas.service
-systemctl restart gvmd
+restart_ctl() {
+    systemctl restart $1
+}
+
+restart_ctl gsad.service
+restart_ctl ospd-openvas.service
+restart_ctl gvmd.service
